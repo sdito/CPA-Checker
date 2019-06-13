e10599f1db5b91cf9f15245f0b5f9268820b81c2
Steven Dito <sfdito@gmail.com>
2019-06-12 19:08:12 -0700

    .

--- cpaChecker/View Controllers/PopUpVC.swift
+++ cpaChecker/View Controllers/PopUpVC.swift
@@ -32,11 +32,41 @@ class PopUpVC: UIViewController {
     }
 
     @IBAction func donePressed(_ sender: Any) {
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        let realmClass = RealmNewClass()
+        var isAccounting: Bool
+        var isBusiness: Bool
+        var isEthics: Bool
+        if accounting.selectedSegmentIndex == 0 {
+            isAccounting = true
+        } else {
+            isAccounting = false
+        }
+        if business.selectedSegmentIndex == 0 {
+            isBusiness = true
+        } else {
+            isBusiness = false
+        }
+        if ethics.selectedSegmentIndex == 0 {
+            isEthics = true
+        } else {
+            isEthics = false
+        }
+        realmClass.courseNum = (courseNameTextEntry.text ?? "")
+        realmClass.numUnits = (Int(numberUnitsTextEntry.text ?? "") ?? 0)
+        realmClass.isAccounting = isAccounting
+        realmClass.isBusiness = isBusiness
+        realmClass.isEthics = isEthics
+        try! realm.write {
+            realm.add(realmClass)
+        }
         
+        delegate.removeBlurViews()
+        delegate.resetTableData()
+        view.removeFromSuperview()
     }
     @IBAction func exitPressed(_ sender: Any) {
         delegate.removeBlurViews()
-        delegate.resetTableData()
         view.removeFromSuperview()
     }
     
